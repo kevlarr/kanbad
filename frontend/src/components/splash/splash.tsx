52c3d337403f542fa98c005dc3b919c8a5b7e013
kevlarr <echo1+git@dvnl.net>
2018-12-04 23:45:09 -0500

    Splash

--- frontend/src/components/splash/splash.tsx
+++ frontend/src/components/splash/splash.tsx
@@ -1,5 +1,17 @@
 import * as React from 'react';
+import './splash.scss';
 
 export default () => (
-    <h2>Hey, make stuff!</h2>
+    <div className='Splash'>
+        <h2 className='welcome'>a <span className='hello'>hello</span> from twello!</h2>
+        <p className='caption'>
+            Workspaces give you places to make things like boards and cards... and magic!
+            Create a new one or, if you're really lucky, get a friend to share a workspace with you.
+        </p>
+        <div className='create'>
+            <button className='button'>
+                Create workspace
+            </button>
+        </div>
+    </div>
 );
