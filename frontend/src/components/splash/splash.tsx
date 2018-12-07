7cfef076ab9e63ed98369c3a3a5749e6ab1edd4e
kevlarr <echo1+git@dvnl.net>
2018-12-07 06:04:26 -0500

    Create workspace on frontend

--- frontend/src/components/splash/splash.tsx
+++ frontend/src/components/splash/splash.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
 import './splash.scss';
 
+function createWorkspace() {
+    fetch('/api/v1/workspaces/new', { method: 'POST' })
+        .then(resp => resp.json())
+        .then(({ identifier }) => {
+            window.location.hash = `#/${identifier}`;
+        });
+}
+
 export default () => (
     <div className='Splash'>
         <h2 className='welcome'>a <span className='hello'>hello</span> from twello!</h2>
@@ -9,7 +17,7 @@ export default () => (
             Create a new one or, if you're really lucky, get a friend to share a workspace with you.
         </p>
         <div className='create'>
-            <button className='button'>
+            <button className='button' onClick={createWorkspace}>
                 Create workspace
             </button>
         </div>
