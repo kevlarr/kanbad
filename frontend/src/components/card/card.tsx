d8f94c3210f4529584ac7e85d94739d0815de7d1
kevlarr <echo1+git@dvnl.net>
2018-12-10 11:50:35 -0500

    Edit/delete card on front end

--- frontend/src/components/card/card.tsx
+++ frontend/src/components/card/card.tsx
@@ -8,17 +8,94 @@ interface Props {
 }
 
 interface State {
+    editing: boolean;
+    bodyValue: string;
+    titleValue: string;
 }
 
 export class Card extends React.Component<Props, State> {
-    render() {
-        // Rotate randomly between -3 and 3 deg
-        const rotation = Math.round(Math.random() * 6) - 3;
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            editing: false,
+            bodyValue: props.card.body,
+            titleValue: props.card.title,
+        };
+    }
+
+    edit() {
+        this.setState({ editing: true });
+    }
+
+    delete() {
+        deleteCard(this.props.card);
+    }
+
+    save() {
+        updateCard({
+            ...this.props.card, 
+            title: this.state.titleValue,
+            body: this.state.bodyValue,
+        });
+        this.setState({ editing: false });
+    }
+
+    cancel() {
+        this.setState({
+            editing: false,
+            bodyValue: this.props.card.body,
+            titleValue: this.props.card.title,
+        });
+    }
+
+    renderContent() {
+        if (this.state.editing) {
+            return (
+                <form className='card-form'>
+                    <input
+                        type='text'
+                        autoFocus={true}
+                        className='title-input'
+                        defaultValue={this.props.card.title}
+                        onBlur={(evt) => this.setState({ titleValue: evt.target.value })}
+                    />
+                    <textarea
+                        className='body-input'
+                        defaultValue={this.state.bodyValue}
+                        placeholder='Give the card some details...'
+                        onBlur={(evt) => this.setState({ bodyValue: evt.target.value })}
+                    />
+                    <button
+                        type='submit'
+                        className='button sm save'
+                        onClick={() => this.save()}
+                    >Save</button>
+                    <button
+                        className='button sm cancel'
+                        onClick={() => this.cancel()}
+                    >Cancel</button>
+                </form>
+            );
+        }
 
         return (
-            <div className='Card' style={{transform: `rotate(${rotation}deg)`}}>
+            <div className='card-content'>
+                <h4 className='card-title'>{this.props.card.title}</h4>
+                <p className='card-body'>{this.props.card.body}</p>
+                <div className='card-edit'>
+                    <a onClick={() => this.edit()}>Edit</a>
+                    <a onClick={() => this.delete()}>Delete</a>
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div className='Card'>
                 <div className='card-pin'></div>
-                {this.props.card.title}
+                {this.renderContent()}
             </div>
         );
     }
