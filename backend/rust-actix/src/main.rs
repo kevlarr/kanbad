8e9de0d2db0a2f93eeb2df4c9a69136d28114fdc
kevin larrabee <echo1+git@dvnl.net>
2019-01-03 21:04:26 -0500

    "hello" endpoint

--- backend/rust-actix/src/main.rs
+++ backend/rust-actix/src/main.rs
@@ -1,3 +1,25 @@
+use actix_web::{
+    http::Method,
+    server,
+    App,
+    HttpRequest,
+    HttpResponse
+};
+
+fn hello(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().body("
+        <!doctype html>
+        <html>
+            <head><title>Hello, Twello!</title></head>
+            <body><h1>Hello, Twello!</h1></body>
+        </html>
+    ")
+}
+
 fn main() {
-    println!("Hello, world!");
+    server::new(|| {
+        App::new().route("hello", Method::GET, hello)
+    }).bind("127.0.0.1:1337")
+        .unwrap()
+        .run();
 }
